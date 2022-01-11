b78b27b81ed7ec19aea734d3a0074757279e726f
Logan Woolf <loganwoolf@gmail.com>
2022-01-11 02:11:14 +0000

    Create head function
    Returns the first item in an array

---
+++ head.js
@@ -0,0 +1,18 @@
+const assertEqual = function(actual, expected) {
+  actual === expected
+    ? console.log(`👌 Assertion Passed 👌: ${actual} === ${expected}`)
+    : console.log(`💥 Assertion Failed 💥: ${actual} !== ${expected}`);
+};
+
+const head = function(arr) {
+  if (arr.length === 0) {
+    return undefined
+  } else if (arr.length > 0) {
+    return arr[0]
+  }
+}
+
+assertEqual(head([5,7,9]), 5)
+assertEqual(head([5,7,9]), 4)
+assertEqual(head(['Logan','Frank','Jim']), 'Logan')
+assertEqual(head(['Logan','Frank','Jim']), 'Frank')
