673c75744ff8120e2b10503e71a7d908136c64de
Logan Woolf <loganwoolf@gmail.com>
2022-01-12 05:48:14 +0000

    Lint head.js

--- head.js
+++ head.js
@@ -6,13 +6,13 @@ const assertEqual = function(actual, expected) {
 
 const head = function(arr) {
   if (arr.length === 0) {
-    return undefined
+    return undefined;
   } else if (arr.length > 0) {
-    return arr[0]
+    return arr[0];
   }
-}
+};
 
-assertEqual(head([5,7,9]), 5)
-assertEqual(head([5,7,9]), 4)
-assertEqual(head(['Logan','Frank','Jim']), 'Logan')
-assertEqual(head(['Logan','Frank','Jim']), 'Frank')
+assertEqual(head([5,7,9]), 5);
+assertEqual(head([5,7,9]), 4);
+assertEqual(head(['Logan','Frank','Jim']), 'Logan');
+assertEqual(head(['Logan','Frank','Jim']), 'Frank');
